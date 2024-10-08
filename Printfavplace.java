2d14558cffc17c306d1a72e863cd1ee861f04b54
G8-2110990159 <amarbir0159.be21@chitkara.edu.in>
2024-10-08 14:53:14 +0530

    [AMAR] ADD: added prgram print three place

---
+++ Printfavplace.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Printfavplace{
+
+public static  void main(String [] args){
+
+Scanner sc  = new Scanner(System.in);
+
+System.out.println("write your first place ");
+
+String place1 =  sc.nextLine();
+
+System.out.println("write your second place  ");
+
+String place2 = sc.nextLine();
+
+System.out.println("write your third place ");
+
+String place3 = sc.nextLine();
+
+System.out.println(place1);
+
+System.out.println(place2);
+
+System.out.println(place3);
+
+	}
+}
+
